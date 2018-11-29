c8f406bad72ed823e05862471de693a1ff3f6c1e
XaRDaX <babask8er@gmail.com>
2018-11-29 11:45:00 +0100

    First Issue

---
+++ DIN_pipe_specs.py
@@ -0,0 +1,11 @@
+import pint
+
+u = pint.UnitRegistry()
+
+class _11866_b:
+    def __init_subclass__(dn6):
+        dn6.dn = 6
+        dn6.id = 7*u.mm
+        dn6.od = 10.2 * u.mm
+        dn6.wall_thk = 1.6 * u.mm
+        dn6.area = 38.5 * u.mm**2
